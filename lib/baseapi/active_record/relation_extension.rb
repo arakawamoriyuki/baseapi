77b46385d744be593d2fd5375a583fa0937cc3d9
新川 盛幸 <arakawa_moriyuki@ca-adv.co.jp>
2015-07-29 17:09:45 +0900

    translate

--- lib/baseapi/active_record/relation_extension.rb
+++ lib/baseapi/active_record/relation_extension.rb
@@ -1,29 +1,25 @@
 module ActiveRecordRelationExtension
-  # ----- model relation object methods -----
-  # def relation_object_method
-  # end
-  # Model.all.relation_object_method
 
-  # カラムの検索
-  # @param  Hash    params  検索パラメータ
+  # column search
+  # @param  Hash    params
   def filtering!(params)
     models = self
     associations = self.model.get_associations()
     params.each do |key, value|
       if key.present? and value.present?
         if column_names.include?(key)
-          # 配列に対応する
+          # array change
           values = value.instance_of?(Array) ? value : [value]
           values.reject!(&:blank?)
-          # カラム用関数が定義されていればそれを使用
+          # call function
           function_name = self.model.methods.include?("_where_#{key}".to_sym) ? "_where_#{key}" : '_where'
           models = self.model.send(function_name, models, key, values)
         end
 
-        # belongs_to, has_manyの検索
+        # belongs_to, has_many search
         associations.keys.each do |association|
           if associations[association].include?(key) and value.instance_of?(ActionController::Parameters)#hash型はActionController::Parameters
-            # カラム用関数が定義されていればそれを使用
+            # call function
             function_name = self.model.methods.include?("_#{association}_#{key}".to_sym) ? "_#{association}_#{key}" : "_#{association}"
             models = self.model.send(function_name, models, key, value)
           end
@@ -33,8 +29,8 @@ module ActiveRecordRelationExtension
     return models
   end
 
-  # ページャー
-  # @param  Hash    params  検索パラメータ
+  # pager
+  # @param  Hash    params
   def paging!(params)
     count = params[:count].present? ? params[:count].to_i : -1;
     page = params[:page].present? ? params[:page].to_i : 1;
@@ -48,8 +44,8 @@ module ActiveRecordRelationExtension
     end
   end
 
-  # ソート
-  # @param  Hash    params  検索パラメータ
+  # sort
+  # @param  Hash    params
   def sorting!(params)
     if params[:order].present? and params[:orderby].present?
       order!({params[:orderby] => params[:order]})
